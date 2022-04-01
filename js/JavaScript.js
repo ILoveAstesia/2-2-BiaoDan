5ed84f746bd60fabe6dc35802c2793bb325548d1
ILoveAstesia <1324677263@qq.com>
2022-04-01 10:27:14 +0800

    first commit

---
+++ js/JavaScript.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    var np = document.getElementById("new_password").value;
+    var rp = document.getElementById("re_password").value;
+    if(rp!=np)
+    {
+        window.alert("不一致");
+    }
+    else;
+}
